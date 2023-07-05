4b0c25796ab5ee6d79dfb1f4afd983c4e238ab4b
Rikkert Koppes <rikkert@rikkertkoppes.com>
2023-07-05 14:54:45 +0200

    fico, project setup

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import postcssImport from "postcss-import";
+
+const packageJson = require("./package.json");
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: packageJson.main,
+                format: "cjs",
+                sourcemap: true,
+            },
+            // {
+            //     file: packageJson.module,
+            //     format: "esm",
+            //     sourcemap: true,
+            // },
+        ],
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+            postcss({
+                plugins: [postcssImport()],
+            }),
+        ],
+    },
+];
