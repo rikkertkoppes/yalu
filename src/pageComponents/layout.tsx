da8bc4d8c3709d605e4567ccb3dd7afb5fc71537
Rikkert Koppes <rikkert@rikkertkoppes.com>
2024-07-31 21:26:24 +0200

    more examples

--- src/pageComponents/layout.tsx
+++ src/pageComponents/layout.tsx
@@ -1,15 +1,24 @@
+import Link from "next/link";
 import { Frame, Top, Filler, Cap, Left, Button } from "../components";
 
 export default function Layout({ children, title }) {
     return (
-        <Frame padding={20} left={150} top={40} className="lcars-gray">
+        <Frame flex padding={20} left={150} top={40} className="lcars-gray">
             <Top>
                 <Filler flex />
                 {title}
                 <Cap wide />
             </Top>
             <Left>
-                <Button>Frames</Button>
+                <Link href="/">
+                    <Button>Frames</Button>
+                </Link>
+                <Link href="/buttons">
+                    <Button>Buttons</Button>
+                </Link>
+                <Link href="/example">
+                    <Button>Example</Button>
+                </Link>
                 <Filler flex />
             </Left>
             <div className="lcars-reset">{children}</div>
