c0975fb7b4c7cd64cbd2262ef0e9234d9d0e0f6c
Rikkert Koppes <rikkert@rikkertkoppes.com>
2024-08-02 17:30:58 +0200

    add document for docs

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="nl">
+                <Head>
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Abel&family=Anton&display=swap"
+                        rel="stylesheet"
+                    ></link>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
