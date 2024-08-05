390dbf16c29dfceeb3474f6433109e5f45543b6d
Rikkert Koppes <rikkert@rikkertkoppes.com>
2024-08-05 11:31:49 +0200

    try to fix basepath for pages

--- next.config.js
+++ next.config.js
@@ -3,9 +3,11 @@ const makeWithMDX = require("@next/mdx");
 const withMDX = makeWithMDX();
 
 const isProd = process.env.NODE_ENV === "production";
+console.log("env", process.env.NODE_ENV, isProd);
 
 module.exports = withMDX({
     output: "export",
     pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-    basePath: isProd ? "/yalu" : undefined,
+    // basePath: isProd ? "/yalu" : undefined,
+    basePath: "/yalu",
 });
