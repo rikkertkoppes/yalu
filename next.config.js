7589113f90a46a49e6522ef6392a60a43a38886b
Rikkert Koppes <rikkert@rikkertkoppes.com>
2024-08-05 11:26:33 +0200

    fix basepath for github pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@ const makeWithMDX = require("@next/mdx");
 
 const withMDX = makeWithMDX();
 
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = withMDX({
     output: "export",
     pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+    basePath: isProd ? "/yalu" : undefined,
 });
