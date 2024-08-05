d0ba6df2473d3a24dc82a617aeb654866f90d9c6
Rikkert Koppes <rikkert@rikkertkoppes.com>
2024-08-05 11:08:44 +0200

    setup github pages

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const makeWithMDX = require("@next/mdx");
 const withMDX = makeWithMDX();
 
 module.exports = withMDX({
+    output: "export",
     pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 });
